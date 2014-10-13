1d65f8bcf3e6c22cfb2b95bd16180df7a48d240e
Garrett Capaccioli <garrettcap@comcast.net>
2014-10-14 02:33:25 +1300

    added logging functionality (WIP)

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 import os
 import os.path as path
 import sys
@@ -5,32 +6,79 @@ import shutil
 import hashlib
 import getopt
 import pprint
-
+import logging
+import logging.handlers
 from cPickle import dump, load
 
 myArchive = os.path.expanduser("~/Desktop/myBackup")
 myObjects = myArchive + "/objects"
 myIndex = myArchive + "/index"
 
+# LOGGER
+#==============================================================
+PROGRAM_NAME = "myBackup"
+LOG_FILENAME = PROGRAM_NAME + '.log'
+CONSOLE_LOG_LEVEL = logging.ERROR
+FILE_LOG_LEVEL = logging.INFO
+# Only show errors to the console
+# but log info messages to the logfile
+logger = logging.getLogger(PROGRAM_NAME)
+logger.setLevel(logging.DEBUG)
+#====================================================================================
+# FILE-BASED LOG
+# Create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#formatter = logging.Formatter('%(levelname)s - %(message)s')
+fh = logging.handlers.RotatingFileHandler(LOG_FILENAME, backupCount=5)  # Rotating log
+fh.setLevel(FILE_LOG_LEVEL)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+# logger.handlers[0].doRollover()  # Roll to new logfile on application start
+# Add timestamp
+#logger.info('\n---------\nLog started on %s.\n---------\n' % time.asctime())
+#=================================================================================
+# CONSOLE HANDLER - can have a different loglevel and format to the file-based log
+ch = logging.StreamHandler()
+ch.setLevel(CONSOLE_LOG_LEVEL)
+formatter = logging.Formatter('%(message)s')
+# simpler display format
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+#=================================================================================
+# In APPLICATION CODE, use whichever of the following is appropriate:
+# logger.debug('debug message ' + time.ctime())
+# logger.info('info message ' + time.ctime())
+# logger.warn('warn message')
+# logger.error('error message')
+# logger.critical('critical message')
+#=================================================================================
+# Test Logger
+# f = open("myBackup.log")
+# s = f.read()
+# print s
+#===============================================================================
+# END LOGGER
 
+# beginning of program
 def init():  # creates archive folder and index files
     if path.exists(myArchive) and path.isdir(myArchive):
-        print("Archive Directory already exists.")
+        logger.error('Archive directory already exists')
     else:
         user_input = raw_input("Would you like to create an archive? ")
         if user_input.lower() == "yes" or user_input.lower() == "y":
             create_archive()
         else:
-            print("Archive not created.")
+            logger.info("Archive not created")
+            print ("Archive not created")
 
 
-def create_archive():
+def create_archive():  # creates the backup archive and log file
     index_init = {}  # initializes blank index dictionary
-
     os.mkdir(myArchive, 0777)  # creates new archive directory
     os.mkdir(myArchive + "/objects", 0777)  # creates new object subdirectory
     dump(index_init, open(myArchive + "/index", 'wb'))  # creates empty? index file
-    print("Archive successfully created.")
+    print("Archive successfully created")
+    logger.info("Archive successfully created")
 
 
 def create_file_signature(filename):
@@ -47,12 +95,16 @@ def create_file_signature(filename):
         signature = hash_value
     except IOError:
         signature = None
+        logger.error(IOError)
     except OSError:
         signature = None
+        logger.error = OSError
     finally:
         if temp_file:
             temp_file.close()
+            logger.info("Hashing successful " + filename)
             return signature
+
 
 def make_directories(paths):
     if not os.path.exists(paths):
@@ -64,7 +116,7 @@ def index_check_and_add(hash_name, relative_path, file_name, index_file):
     index_file[hash_name] = file_path
     return file_path
 
-
+"""
 def index_rename_to_original(restore_fullpath, index_file, destination_directory, file1):
     print index_file
     print file1
@@ -74,18 +126,19 @@ def index_rename_to_original(restore_fullpath, index_file, destination_directory
     new_file_fullpath = os.path.join(destination_directory, index_file_name)
     print new_file_fullpath
     os.rename(restore_fullpath, new_file_fullpath)
-
+"""
 
 def store_backup(user_input_directory):
     index_file = load((open(myIndex, 'rb')))  # loads index file
     files_not_added_count = 0  # file not added counter duh!
     new_files_added = []  # really....
-    print("Yay!")  # logger
+    logger.info("Backup init successful")  # logger
 
     for root, dirs, files in os.walk(user_input_directory):
         for file_name in files:
                 original_path = os.path.join(root, file_name)
                 hash_name = create_file_signature(original_path)  # creates a hash for the file
+                logger.info(hash_name)
                 if hash_name in index_file.values():  # checks file against index
                     files_not_added_count += 1
                 else:
@@ -97,7 +150,7 @@ def store_backup(user_input_directory):
                     shutil.copy2(original_path, file_backup_path)  # copies files...
                     #  adds files to index and new files added list
                     new_file = index_check_and_add(hash_name, relative_path, file_name, index_file)
-                    new_files_added.append(new_file)
+                    new_files_added.append(new_file) # adds files to
 
     dump(index_file, open(myIndex, "wb"))  # dumps info into index file
     print("Files not added: " + str(files_not_added_count))
@@ -130,9 +183,9 @@ def get_file(desired_file):
 #recovers all files from archive
 def restore_files(destination_directory):
     index_file = load((open(myIndex, 'rb')))  # loads index file
-    make_directories(destination_directory)
+    make_directories(destination_directory)  # makes directories
     os.chdir(destination_directory)
-    for root, dirs, files in os.walk(myObjects):
+    for root, dirs, files in os.walk(myObjects):  # walks through backup directory
         for file_name in files:
             file_fullpath = os.path.join(root, file_name)  # creates the fullpath to backup folder files
             new_filepath = index_file.get(file_name)  # retrives the file values from dict
@@ -140,16 +193,16 @@ def restore_files(destination_directory):
             make_directories(head)  # makes required directories if non-existent
             shutil.copy2(file_fullpath, new_filepath)  # copies files to restore and renames
 
-       # backup_fullpath = os.join.path(myObjects, original_backup)
-       # restore_fullpath = os.path.join(destination_directory, files)  # creates filepath for restore directory
-       # index_rename_to_original(restore_fullpath, index_file, destination_directory, files)  # renames files to regular names
+# backup_fullpath = os.join.path(myObjects, original_backup)
+# restore_fullpath = os.path.join(destination_directory, files)  # creates filepath for restore directory
+# index_rename_to_original(restore_fullpath, index_file, destination_directory, files)  # renames files to regular names
 
 
-def list_all_items():
+def list_all_items():  # lists all files in backup folder
     index_file = load((open(myIndex, 'rb')))
     for files in index_file.itervalues():
         path.join(files)
-        pprint.pprint(files)
+        pprint.pprint(files) # prings filepath
 
 def main(argv):  # command line interpreter
     try:
@@ -160,6 +213,7 @@ def main(argv):  # command line interpreter
         print("            [-s or --store <directory_path> to create a backup of directory in backup folder]")
         print("            [-r or --restore <destination_directory> to restore entire backup into destination]")
         print("            [-l or --list to list all files currently in backup folder]")
+        print("            [-g or -get <'filename'> restores individual file to current directory]")
         sys.exit()
     for opt, arg in opts:
         if opt == "-h":
@@ -167,6 +221,7 @@ def main(argv):  # command line interpreter
             print("            [-s or --store <directory_path> to create a backup of directory in backup folder]")
             print("            [-r or --restore <destination_directory> to restore entire backup into destination]")
             print("            [-l or --list to list all files currently in backup folder]")
+            print("            [-g or -get <'filename'> restores individual file to current directory]")
             sys.exit()
         elif opt in ("-i", "--init"):
             init()
@@ -179,6 +234,9 @@ def main(argv):  # command line interpreter
             stored_directory = arg
             store_backup(stored_directory)
         elif opt in ("-g" or "--get"):
+            if not type(arg) is str:
+                print("-g or --get <'filename'> (filename must be in quotes)")
+                sys.exit()
             get_file(arg)
         else:
             print('ERROR')

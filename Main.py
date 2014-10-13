ecc81b04303d17aef74a202a8bd29d8cd9e733ca
Garrett Capaccioli <garrettcap@comcast.net>
2014-10-13 17:27:42 +1300

    new commit

--- Main.py
+++ Main.py
@@ -55,8 +55,11 @@ def create_file_signature(filename):
             return signature
 
 
-def index_check_and_add(hash_name, file_name, index_file):
-    index_file[hash_name] = file_name
+def index_check_and_add(hash_name, relative_path, file_name, index_file):
+    file_path = os.path.join(relative_path, file_name)
+    index_file[file_path] = hash_name
+    return file_path
+
 
 def index_rename_to_original(restore_fullpath, index_file, destination_directory, file1):
     print index_file
@@ -77,38 +80,46 @@ def store_backup(user_input_directory):
 
     for root, dirs, files in os.walk(user_input_directory):
         for file_name in files:
-                original_path = path.join(user_input_directory, file_name)
+                original_path = os.path.join(root, file_name)
+
                 hash_name = create_file_signature(original_path)  # creates a hash for the file
                 if hash_name in index_file.values():  # checks file against index
                     files_not_added_count += 1
                 else:
-                    backup_path = path.join(myObjects, file_name)
-                    shutil.copy2(original_path, myObjects)  # copies files...
-                    os.rename(backup_path, myObjects + "/" + hash_name)  # renames files to hash equil
-                    index_check_and_add(hash_name, file_name, index_file)  # adds hash/filename to index
-                    new_files_added.append(file_name)
-
+                    relative_path = os.path.relpath(root, user_input_directory)
+                    os.chdir(myObjects)
+                    if not os.path.exists(relative_path):
+                        os.makedirs(relative_path)
+                    file_backup_path = os.path.join(relative_path, hash_name)
+                    shutil.copy2(original_path, file_backup_path)  # copies files...
+                    #  adds files to index and new files added list
+                    new_file = index_check_and_add(hash_name, relative_path, file_name, index_file)
+                    new_files_added.append(new_file)
+                    
     dump(index_file, open(myIndex, "wb"))  # dumps info into index file
     print("Files not added: " + str(files_not_added_count))
     print("New files added: ")
     pprint.pprint(new_files_added)
 
+
 #store_backup("/home/gar/Desktop/moop") # use for testing purposes
 
 
 #recovers a single file from archive
-def get_file():
-    return 0
+def get_file(desired_file):
+    index_file = load((open(myIndex, 'rb')))
+
 
 
 #recovers all files from archive
 def restore_files(destination_directory):
-    index_file = load((open(myIndex, 'rb')))
+    index_file = load((open(myIndex, 'rb')))  # loads index file
     for files in index_file.iterkeys():
-        original_fullpath = os.path.join(myObjects, files)
-        shutil.copy2(original_fullpath, destination_directory)
-        restore_fullpath = os.path.join(destination_directory, files)
-        index_rename_to_original(restore_fullpath, index_file, destination_directory, files)
+            shutil.copytree(files, destination_directory) # copies files into restore directory
+            shutil.copy2(files, destination_directory)
+
+       # restore_fullpath = os.path.join(destination_directory, files)  # creates filepath for restore directory
+       # index_rename_to_original(restore_fullpath, index_file, destination_directory, files)  # renames files to regular names
 
 
 def list_all_items():
@@ -124,11 +135,15 @@ def main(argv):  # command line interpreter
         print("Must be in the following format")
         print("Options are [-i or --init to initialize and create backup folder]")
         print("            [-s or --store <directory_path> to create a backup of directory in backup folder]")
+        print("            [-r or --restore <destination_directory> to restore entire backup into destination]")
+        print("            [-l or --list to list all files currently in backup folder]")
         sys.exit()
     for opt, arg in opts:
         if opt == "-h":
             print("Options are [-i or --init to initialize and create backup folder]")
-            print("            [-s or --store <directory_path> to create a backup of directory in backup folder")
+            print("            [-s or --store <directory_path> to create a backup of directory in backup folder]")
+            print("            [-r or --restore <destination_directory> to restore entire backup into destination]")
+            print("            [-l or --list to list all files currently in backup folder]")
             sys.exit()
         elif opt in ("-i", "--init"):
             init()
